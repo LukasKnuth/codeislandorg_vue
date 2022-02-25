99bc41afb169a6ef6956033f41881bef486e5d3f
Lukas Knuth <lukas.knuth@betterdoc.de>
2022-02-25 09:11:29 +0100

    Built simple counter page with store.

---
+++ src/pages/counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>
+    <button @click="state.increment">++</button>
+    <button @click="state.decrement">--</button>
+    Counter: {{state.counter}}<span></span>
+  </span>
+</template>
+
+<script>
+import { useCounterStore } from '@/stores/counter'
+import { useRoute } from 'vue-router'
+ 
+export default {
+  setup() {
+    const counter = useCounterStore()
+    const route = useRoute()
+
+    const initialValue = route.query["counter"] || 0
+    counter.$patch({counter: initialValue})
+
+    return {state: counter}
+  }
+}
+</script>
+
+<style>
+
+</style>
