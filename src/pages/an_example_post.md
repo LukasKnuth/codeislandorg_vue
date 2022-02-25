256d51cc1070f12b8543837549686599ba4ece0f
Lukas Knuth <lukas.knuth@betterdoc.de>
2022-02-25 09:22:16 +0100

    Added automatic markdown parsing.

---
+++ src/pages/an_example_post.md
@@ -0,0 +1,17 @@
+---
+name: An example post
+---
+
+# Hello World
+
+This is the long awaited example Post on my new Blog!
+
+```javascript
+const greet = (name) => {
+  console.log("Hello $name")
+}
+
+greet("Lukas")
+```
+
+Some additional stuff goes here then.
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import voie from 'vite-plugin-voie'
+import markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), voie()],
+  plugins: [
+    vue({include: [/\.vue$/, /\.md$/]}),
+    voie({extensions: ['vue', 'md']}),
+    markdown()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
